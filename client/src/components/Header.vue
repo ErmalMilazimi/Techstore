461652d6bae56a74525e3c4f7453adff16a1443a
ErmalMilazimi <emilazimi11@gmail.com>
2022-01-26 17:37:15 +0100

    Header and Nav added

---
+++ client/src/components/Header.vue
@@ -0,0 +1,162 @@
+<template>
+  <Nav></Nav>
+  <div class="header" :class="secondHeader">
+    <img
+      class="header-banner"
+      :src="require(`@/assets/${backgroundUrl}`)"
+      alt=""
+    />
+    <div class="hide">
+      <div class="header-text-container">
+        <h3 class="header-title">Mire se vini ne TechStore</h3>
+        <p class="header-text">
+          Ketu do gjeni me shume se 20,000 produkte teteknologjise me te fundit
+          nga shume brende boterore. Shikoni me shume dhe mahnituni nga
+          produktet tona.
+        </p>
+      </div>
+      <div class="header-arrowContainer">
+        <img
+          @click="goto()"
+          class="arrowIcon"
+          src="../assets/arrowIcon.svg"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Nav from "./Nav.vue";
+export default {
+  name: "Header",
+  props: {
+    secondHeader: String,
+    backgroundUrl: String,
+  },
+  components: {
+    Nav,
+  },
+  methods: {
+    goto() {
+      window.scrollTo(0, window.innerHeight);
+    },
+  },
+};
+</script>
+<style scoped>
+.header {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background-color: #b2ffff;
+  padding-top: 72px;
+  overflow: hidden;
+}
+.header.secondHeader {
+  height: 50vh;
+}
+.header.secondHeader .hide {
+  display: none;
+}
+.header .header-banner {
+  width: 100%;
+  min-width: 800px;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%) scale(1);
+  animation: animBannerText 0.5s linear;
+}
+
+.header.header.secondHeader .header-banner {
+  top: -80px;
+}
+@keyframes animBannerText {
+  0% {
+    opacity: 0;
+    transform: translateX(-50%) scale(1.5);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(-50%) scale(1);
+  }
+}
+.header-text-container {
+  position: absolute;
+  bottom: 10vh;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  animation: animHeaderText 0.5s linear;
+}
+@keyframes animHeaderText {
+  0% {
+    opacity: 0;
+    transform: translate(-50%, 150%) scale(0.7);
+  }
+  100% {
+    opacity: 1;
+    transform: translate(-50%, -50%) scale(1);
+  }
+}
+.header-title {
+  color: #002244;
+  font-size: 45px;
+}
+.header-text {
+  margin-top: 20px;
+  font-size: 18px;
+  color: #002244;
+}
+.header-arrowContainer {
+  position: absolute;
+  left: 50%;
+  bottom: 30px;
+  transform: translate(-50%, 0);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.header .arrowIcon {
+  width: 50px;
+  height: 50px;
+  transform: translateY(0) rotate(-90deg);
+  animation: animArrowIcon 1s linear infinite alternate;
+  cursor: pointer;
+}
+.header .arrowIcon:hover {
+  animation: none;
+  transform: translateY(0) rotate(-90deg) scale(1.2);
+}
+@keyframes animArrowIcon {
+  0% {
+    transform: translateY(0) rotate(-90deg);
+  }
+  100% {
+    transform: translateY(20px) rotate(-90deg);
+  }
+}
+
+@media only screen and (max-width: 1024px) {
+  .header-text-container {
+    width: 80%;
+  }
+  .header-title {
+    font-size: 30px;
+  }
+  .header-text {
+    font-size: 14px;
+  }
+}
+@media only screen and (max-width: 686px) {
+  .header-text-container {
+    bottom: 5vh;
+  }
+  .header-title {
+    font-size: 24px;
+  }
+  .header-text {
+    font-size: 13px;
+  }
+}
+</style>
---
+++ client/src/components/Nav.vue
@@ -0,0 +1,90 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light py-3 px-5" :class="active">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/"
+        ><img
+          src="../assets/companyIcon.svg"
+          alt=""
+          width="30"
+          height="24"
+          class="d-inline-block align-text-top"
+        />
+        TechStore
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/productList"
+              >Products</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/product">About Us</router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "Nav",
+  props: {},
+  data() {
+    return {
+      active: "",
+    };
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      if (window.scrollY > 500) {
+        this.active = "active";
+      } else {
+        this.active = "";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.navbar {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.5);
+  transition: background-color 0.1s ease;
+  z-index: 10;
+}
+.navbar.active {
+  position: fixed;
+  background-color: rgba(255, 255, 255, 0.8);
+  box-shadow: 0px 6px 20px 1px #0000001a;
+}
+.navbar:hover {
+  background-color: rgba(255, 255, 255, 1);
+}
+.navbar-collapse {
+  text-align: center;
+}
+</style>
