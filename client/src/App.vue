5a7d2c82ba8f9ce70778e1319c55f53cb4a3c497
ErmalMilazimi <emilazimi11@gmail.com>
2022-01-26 17:35:56 +0100

    Initial Commit

---
+++ client/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins&family=Roboto&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
+}
+</style>
